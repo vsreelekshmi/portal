f4942f0e04021608ca335504bbc6e29dc227711d
Sreelekshmi V <sreelekshmi.v@tcs.com>
2020-01-30 09:57:17 +0530

    initial commit

---
+++ js/controller.js
@@ -0,0 +1,25 @@
+var app = angular.module('mainApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		templateUrl: 'login.html'
+	})
+	.when('/dashboard',{
+		templateUrl: 'dashboard.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
+
+
+app.controller('loginCtrl',function($scope, $location){
+	$scope.submit = function(){
+		var uname = $scope.username;
+		var password = $scope.password;
+		if($scope.username == 'admin' && $scope.password == 'admin'){
+			$location.path('/dashboard');
+		}
+	};
+});
